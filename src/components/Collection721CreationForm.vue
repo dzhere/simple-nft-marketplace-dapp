aba726391ca4df75c2764b4fd69c6fd7504ba04c
dzhere <dzgamech@gmail.com>
2022-02-01 21:25:27 +0200

    Add form for creation of ERC-721 contract

---
+++ src/components/Collection721CreationForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-form>
+            <b-form-group label="Collection name">
+                <b-form-input placeholder="Enter collection name" required></b-form-input>
+            </b-form-group>
+            <b-form-group label="Collection symbol">
+                <b-form-input placeholder="Enter collection symbol" required></b-form-input>
+            </b-form-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import { BForm, BFormGroup, BFormInput } from 'bootstrap-vue'
+
+export default {
+    name: 'Collection721CreationForm',
+    components: { BForm, BFormGroup, BFormInput },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ src/views/Launchpad.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b-form-select v-model="collectionType" :options="collectionTypesOptions"></b-form-select>
+        <collection-721-creation-form v-if="collectionType === 721" />
+    </div>
+</template>
+
+<script>
+import { BFormSelect } from 'bootstrap-vue'
+import Collection721CreationForm from '@/components/Collection721CreationForm.vue'
+
+export default {
+    name: 'Launchpad',
+    components: { BFormSelect, Collection721CreationForm, Collection721CreationForm },
+    data() {
+        return {
+            collectionType: null,
+            collectionTypesOptions: [
+                { value: null, text: 'Please, select collection type' },
+                { value: 721, text: 'ERC-721' },
+                { value: 1155, text: 'ERC-1155' }
+            ]
+        }
+    }
+}
+</script>
