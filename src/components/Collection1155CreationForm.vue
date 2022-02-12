bb1af5d263ff4bca34e1ff6c20d0227bac133bbb
dzhere <dzgamech@gmail.com>
2022-02-12 23:38:19 +0200

    Add form for ERC-1155 collection creation

---
+++ src/components/Collection1155CreationForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-form>
+            <b-form-group label="URI">
+                <b-form-input placeholder="Enter URI" required></b-form-input>
+            </b-form-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import { BForm, BFormGroup, BFormInput } from 'bootstrap-vue'
+
+export default {
+    name: 'Collection1155CreationForm',
+    components: { BForm, BFormGroup, BFormInput },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
--- src/views/Launchpad.vue
+++ src/views/Launchpad.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
         <b-form-select v-model="collectionType" :options="collectionTypesOptions"></b-form-select>
-        <collection-721-creation-form v-if="collectionType === 721" />
+        <collection-721-creation-form v-if="isERC721CollectionTypeSelected" />
+        <collection-1155-creation-form v-if="isERC1155CollectionTypeSelected" />
     </div>
 </template>
 
 <script>
 import { BFormSelect } from 'bootstrap-vue'
 import Collection721CreationForm from '@/components/Collection721CreationForm.vue'
+import Collection1155CreationForm from '@/components/Collection1155CreationForm.vue'
 
 export default {
     name: 'Launchpad',
-    components: { BFormSelect, Collection721CreationForm, Collection721CreationForm },
+    components: {
+        BFormSelect,
+        Collection721CreationForm,
+        Collection1155CreationForm
+    },
     data() {
         return {
             collectionType: null,
@@ -20,6 +26,14 @@ export default {
                 { value: 721, text: 'ERC-721' },
                 { value: 1155, text: 'ERC-1155' }
             ]
+        }
+    },
+    computed: {
+        isERC721CollectionTypeSelected () {
+            return this.collectionType === 721
+        },
+        isERC1155CollectionTypeSelected () {
+            return this.collectionType === 1155
         }
     }
 }
